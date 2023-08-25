569deec4d4108262309c8bd4ad6ee8ade41f5837
juwoong kim <wwong7247@gmail.com>
2023-08-25 17:42:14 +0900

    🎉 프로젝트 초기화

---
+++ src/main/java/com/devcourse/be04daangnmarket/Be04DaangnMarketApplication.java
@@ -0,0 +1,13 @@
+package com.devcourse.be04daangnmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Be04DaangnMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Be04DaangnMarketApplication.class, args);
+	}
+
+}
