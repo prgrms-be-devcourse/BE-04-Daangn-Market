55f1839145708edaab69290052db2ed92be9ba28
juwoong kim <wwong7247@gmail.com>
2023-08-30 18:12:28 +0900

    KIM-115 ✨ 게시글 수정 REST API 구현 및 테스트 코드 작성

--- src/main/java/com/devcourse/be04daangnmarket/post/api/PostRestController.java
+++ src/main/java/com/devcourse/be04daangnmarket/post/api/PostRestController.java
@@ -2,7 +2,9 @@ package com.devcourse.be04daangnmarket.post.api;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +26,14 @@ public class PostRestController {
 	@PostMapping
 	public ResponseEntity<PostResponse> createPost(@RequestBody PostRequest request) {
 		PostResponse response = postService.create(request.title(), request.description()
+			, request.price(), request.views(), request.transactionType(), request.category(), request.status());
+
+		return new ResponseEntity<>(response, HttpStatus.OK);
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<PostResponse> updatePost(@PathVariable Long id, @RequestBody PostRequest request) {
+		PostResponse response = postService.update(id, request.title(), request.description()
 			, request.price(), request.views(), request.transactionType(), request.category(), request.status());
 
 		return new ResponseEntity<>(response, HttpStatus.OK);
