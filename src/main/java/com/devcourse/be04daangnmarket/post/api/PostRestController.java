a9246879b7efb37f2d7e3d6bd753a42572816473
juwoong kim <wwong7247@gmail.com>
2023-08-30 17:42:31 +0900

    KIM-115 ✨ 게시글 등록 REST API 구현 및 테스트 코드 작성

---
+++ src/main/java/com/devcourse/be04daangnmarket/post/api/PostRestController.java
@@ -0,0 +1,32 @@
+package com.devcourse.be04daangnmarket.post.api;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devcourse.be04daangnmarket.post.application.PostService;
+import com.devcourse.be04daangnmarket.post.dto.PostRequest;
+import com.devcourse.be04daangnmarket.post.dto.PostResponse;
+
+@RestController
+@RequestMapping("api/v1/posts")
+public class PostRestController {
+
+	public PostRestController(PostService postService) {
+		this.postService = postService;
+	}
+
+	private final PostService postService;
+
+	@PostMapping
+	public ResponseEntity<PostResponse> createPost(@RequestBody PostRequest request) {
+		PostResponse response = postService.create(request.title(), request.description()
+			, request.price(), request.views(), request.transactionType(), request.category(), request.status());
+
+		return new ResponseEntity<>(response, HttpStatus.OK);
+	}
+
+}
