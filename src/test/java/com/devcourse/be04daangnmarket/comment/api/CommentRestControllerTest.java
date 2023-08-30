8f977746720f53d187e7f036742e368b12fbef2e
beombu <beombu13@gmail.com>
2023-08-30 21:54:11 +0900

    KIM-111 :white_check_mark: 댓글 생성기능 테스트코드를 구현하다

---
+++ src/test/java/com/devcourse/be04daangnmarket/comment/api/CommentRestControllerTest.java
@@ -0,0 +1,51 @@
+package com.devcourse.be04daangnmarket.comment.api;
+
+import com.devcourse.be04daangnmarket.comment.application.CommentService;
+import com.devcourse.be04daangnmarket.comment.dto.CommentResponse;
+import com.devcourse.be04daangnmarket.comment.dto.CreateCommentRequest;
+import com.devcourse.be04daangnmarket.common.config.SecurityConfig;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(CommentRestController.class)
+@MockBean(JpaMetamodelMappingContext.class)
+@Import(SecurityConfig.class)
+class CommentRestControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private CommentService commentService;
+
+    @Test
+    void 저장_성공() throws Exception {
+        //given
+        CreateCommentRequest request = new CreateCommentRequest("댓글");
+        CommentResponse response = new CommentResponse("댓글");
+
+        given(commentService.create(request))
+                .willReturn(response);
+
+        //when & then
+        this.mockMvc.perform(post("/api/v1/comments")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isCreated())
+                .andDo(print());
+    }
+}
