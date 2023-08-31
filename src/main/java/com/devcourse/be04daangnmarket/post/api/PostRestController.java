d643f07b4a9c715acbfec9ec681be966b0bcfd69
juwoong kim <wwong7247@gmail.com>
2023-08-31 15:20:33 +0900

    KIM-116 ✨ 게시글 전체조회 REST API 구현 및 테스트 코드를 작성하다

--- src/main/java/com/devcourse/be04daangnmarket/post/api/PostRestController.java
+++ src/main/java/com/devcourse/be04daangnmarket/post/api/PostRestController.java
@@ -1,5 +1,8 @@
 package com.devcourse.be04daangnmarket.post.api;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devcourse.be04daangnmarket.post.application.PostService;
@@ -38,6 +42,15 @@ public class PostRestController {
 		PostResponse response = postService.getPost(id);
 
 		return new ResponseEntity<>(response, HttpStatus.OK);
+	}
+
+	@GetMapping
+	public ResponseEntity<Page<PostResponse>> getAllPost(@RequestParam(defaultValue = "0") int page,
+		@RequestParam(defaultValue = "0") int size) {
+		Pageable pageable = PageRequest.of(page, size);
+		Page<PostResponse> responses = postService.getAllPost(pageable);
+
+		return new ResponseEntity<>(responses, HttpStatus.OK);
 	}
 
 	@PutMapping("/{id}")
