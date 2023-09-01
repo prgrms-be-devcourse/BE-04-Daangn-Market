6152ba4879e02b8270bac846c4548e535562c894
beombu <beombu13@gmail.com>
2023-09-01 13:17:32 +0900

    KIM-118 :sparkles: 댓글 수정기능을 구현하다

--- src/main/java/com/devcourse/be04daangnmarket/comment/api/CommentRestController.java
+++ src/main/java/com/devcourse/be04daangnmarket/comment/api/CommentRestController.java
@@ -7,12 +7,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import com.devcourse.be04daangnmarket.comment.dto.UpdateCommentRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -58,6 +60,13 @@ public class CommentRestController {
         }
 
         Page<CommentResponse> response = commentService.getPage(pageable);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CommentResponse> update(@PathVariable Long id, @RequestBody UpdateCommentRequest request) {
+        CommentResponse response = commentService.update(id, request);
 
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
--- src/main/java/com/devcourse/be04daangnmarket/comment/dto/CommentResponse.java
+++ src/main/java/com/devcourse/be04daangnmarket/comment/dto/CommentResponse.java
@@ -1,5 +1,5 @@
 package com.devcourse.be04daangnmarket.comment.dto;
 
 public record CommentResponse(
-    String comment
+    String content
 ) { }
