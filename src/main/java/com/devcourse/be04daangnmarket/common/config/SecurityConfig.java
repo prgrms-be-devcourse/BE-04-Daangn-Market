8510e042c2b46585be3da6ce5a720cb9e9ce105c
kys0411 <32180846@dankook.ac.kr>
2023-09-04 14:47:34 +0900

    KIM-103 🔐 jwt 설정을 추가하다

--- src/main/java/com/devcourse/be04daangnmarket/common/config/SecurityConfig.java
+++ src/main/java/com/devcourse/be04daangnmarket/common/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.devcourse.be04daangnmarket.common.config;
 
+import com.devcourse.be04daangnmarket.common.jwt.JwtAuthenticationFilter;
+import com.devcourse.be04daangnmarket.common.jwt.JwtTokenProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,11 +9,19 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+	private final JwtTokenProvider jwtTokenProvider;
+
+	public SecurityConfig(JwtTokenProvider jwtTokenProvider) {
+		this.jwtTokenProvider = jwtTokenProvider;
+	}
+
 	@Bean
 	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		return http
@@ -19,6 +29,7 @@ public class SecurityConfig {
 				.requestMatchers(new AntPathRequestMatcher("/**"))
 				.permitAll())
 				.csrf(csrf->csrf.disable())
+				.addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
 			.build();
 	}
 
---
+++ src/main/java/com/devcourse/be04daangnmarket/member/application/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.devcourse.be04daangnmarket.member.application;
+
+import com.devcourse.be04daangnmarket.common.auth.User;
+import com.devcourse.be04daangnmarket.member.domain.Member;
+import com.devcourse.be04daangnmarket.member.repository.MemberRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import static com.devcourse.be04daangnmarket.member.exception.ErrorMessage.NOT_FOUND_USER;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    public UserDetailsServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Member member = memberRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException(NOT_FOUND_USER.getMessage()));
+
+        return new User(member);
+    }
+}
