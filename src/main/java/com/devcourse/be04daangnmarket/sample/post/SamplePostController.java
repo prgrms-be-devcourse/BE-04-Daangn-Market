d44da50be95b8eea9b9374bd09ca25f41d66436f
juwoong kim <wwong7247@gmail.com>
2023-09-06 15:42:24 +0900

    KIM-164 ✨ 게시글 등록 테스트를 위한 샘플 화면을 작성하다

---
+++ src/main/java/com/devcourse/be04daangnmarket/sample/post/SamplePostController.java
@@ -0,0 +1,21 @@
+package com.devcourse.be04daangnmarket.sample.post;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.devcourse.be04daangnmarket.post.domain.Category;
+import com.devcourse.be04daangnmarket.post.domain.TransactionType;
+
+@Controller
+public class SamplePostController {
+
+	@GetMapping("/sample/create/posts")
+	public String sampleMain(Model model){
+		model.addAttribute("categories", Category.values());
+		model.addAttribute("transactionTypes", TransactionType.values());
+
+		return "/sample/sampleCreatePost";
+	}
+
+}
