e5627fc9787bdbc741168ec2264b46e0365dc6fc
juwoong kim <wwong7247@gmail.com>
2023-09-08 09:49:34 +0900

    KIM-000 🚑  Conflict해결시 누락된 정보를 반영하다

--- src/main/java/com/devcourse/be04daangnmarket/common/exception/CommonExceptionHandler.java
+++ src/main/java/com/devcourse/be04daangnmarket/common/exception/CommonExceptionHandler.java
@@ -2,6 +2,7 @@ package com.devcourse.be04daangnmarket.common.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -42,6 +43,6 @@ public class CommonExceptionHandler {
 	public ResponseEntity<String> handleValidationException(MethodArgumentNotValidException exception) {
 		String errorMessage = exception.getBindingResult().getFieldErrors().get(0).getDefaultMessage();
 
-		return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);  
-  
+		return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+	}
 }
