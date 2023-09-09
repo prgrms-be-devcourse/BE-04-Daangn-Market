d2df1e823d5ef4eb17b97904a7e9caad813fb7f6
kys0411 <32180846@dankook.ac.kr>
2023-09-10 01:27:09 +0900

    KIM-152 ✨ 회원 유효성 검증을 추가하다

--- src/main/java/com/devcourse/be04daangnmarket/member/api/MemberRestController.java
+++ src/main/java/com/devcourse/be04daangnmarket/member/api/MemberRestController.java
@@ -4,6 +4,7 @@ import com.devcourse.be04daangnmarket.common.auth.User;
 import com.devcourse.be04daangnmarket.common.jwt.JwtTokenProvider;
 import com.devcourse.be04daangnmarket.member.application.MemberService;
 import com.devcourse.be04daangnmarket.member.dto.MemberDto;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -29,13 +30,13 @@ public class MemberRestController {
     }
 
     @PostMapping
-    public ResponseEntity<MemberDto.Response> signUp(@RequestBody MemberDto.SignUpRequest request) {
+    public ResponseEntity<MemberDto.Response> signUp(@RequestBody @Valid MemberDto.SignUpRequest request) {
         MemberDto.Response response = memberService.signUp(request);
         return new ResponseEntity(response, HttpStatus.CREATED);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<MemberDto.Response> signIn(@RequestBody MemberDto.SignInRequest request) {
+    public ResponseEntity<MemberDto.Response> signIn(@RequestBody @Valid MemberDto.SignInRequest request) {
         MemberDto.Response response = memberService.signIn(request);
 
         String token = jwtTokenProvider.createToken(request.email());
@@ -54,7 +55,7 @@ public class MemberRestController {
     @PutMapping("/{id}")
     public ResponseEntity<MemberDto.Response> updateProfile(@AuthenticationPrincipal User user,
                                                             @PathVariable Long id,
-                                                            @RequestBody MemberDto.UpdateProfileRequest request) {
+                                                            @RequestBody @Valid MemberDto.UpdateProfileRequest request) {
         memberService.validateById(id, user.getId());
         MemberDto.Response response = memberService.updateProfile(id, request.username());
         return new ResponseEntity<>(response, HttpStatus.OK);
--- src/main/java/com/devcourse/be04daangnmarket/member/dto/MemberDto.java
+++ src/main/java/com/devcourse/be04daangnmarket/member/dto/MemberDto.java
@@ -1,24 +1,43 @@
 package com.devcourse.be04daangnmarket.member.dto;
 
 import com.devcourse.be04daangnmarket.member.domain.constant.Status;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import org.apache.logging.log4j.core.config.plugins.validation.constraints.NotBlank;
 
 import java.time.LocalDateTime;
 
 public class MemberDto {
 
     public record SignUpRequest(
+            @NotBlank(message = "닉네임은 필수로 입력하여야 합니다.")
             String username,
+
+            @NotBlank(message = "전화번호는 필수로 입력하여야 합니다.")
+            @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$", message = "전화번호 형식에 맞게 입력해주세요.")
             String phoneNumber,
+
+            @NotBlank(message = "이메일은 필수로 입력하여야 합니다.")
+            @Email(message = "이메일 형식이 잘못되었습니다.")
             String email,
+
+            @NotBlank(message = "비밀번호는 필수로 입력하여야 합니다.")
+            @Size(min = 8, max= 20, message = "비밀번호는 8자 이상, 20자 이하로 작성하여야 합니다.")
             String password
     ) { }
 
     public record SignInRequest(
+            @NotBlank(message = "이메일은 필수로 입력하여야 합니다.")
+            @Email(message = "이메일 형식이 잘못되었습니다.")
             String email,
+
+            @NotBlank(message = "비밀번호는 필수로 입력하여야 합니다.")
             String password
     ) { }
 
     public record UpdateProfileRequest(
+            @NotBlank(message = "닉네임은 필수로 입력하여야 합니다.")
             String username
     ) { }
 
