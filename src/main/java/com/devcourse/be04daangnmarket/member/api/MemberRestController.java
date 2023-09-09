f653f7bd69d9532d747184a6e631b126edaa6734
kys0411 <32180846@dankook.ac.kr>
2023-09-09 13:56:02 +0900

    KIM-128 ✨ 사용자의 판매내역 api를 구현하다

--- src/main/java/com/devcourse/be04daangnmarket/member/api/MemberRestController.java
+++ src/main/java/com/devcourse/be04daangnmarket/member/api/MemberRestController.java
@@ -3,6 +3,12 @@ package com.devcourse.be04daangnmarket.member.api;
 import com.devcourse.be04daangnmarket.common.jwt.JwtTokenProvider;
 import com.devcourse.be04daangnmarket.member.application.MemberService;
 import com.devcourse.be04daangnmarket.member.dto.MemberDto;
+import com.devcourse.be04daangnmarket.post.application.PostService;
+import com.devcourse.be04daangnmarket.post.dto.PostDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,17 +18,21 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/members")
 public class MemberRestController {
+    private final int PAGE_SIZE = 5;
 
     private final MemberService memberService;
+    private final PostService postService;
     private final JwtTokenProvider jwtTokenProvider;
 
-    public MemberRestController(MemberService memberService, JwtTokenProvider jwtTokenProvider) {
+    public MemberRestController(MemberService memberService, PostService postService, JwtTokenProvider jwtTokenProvider) {
         this.memberService = memberService;
+        this.postService = postService;
         this.jwtTokenProvider = jwtTokenProvider;
     }
 
@@ -54,5 +64,12 @@ public class MemberRestController {
                                                             @RequestBody MemberDto.UpdateProfileRequest request) {
         MemberDto.Response response = memberService.updateProfile(id, request.username());
         return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}/sale")
+    public ResponseEntity<Page<PostDto.Response>> getSaleList(@PathVariable Long id, @RequestParam(defaultValue = "0") int page) {
+        Pageable pageable = PageRequest.of(page, PAGE_SIZE, Sort.by("id").descending());
+        Page<PostDto.Response> saleList = postService.getPostByMemberId(id, pageable);
+        return new ResponseEntity<>(saleList, HttpStatus.OK);
     }
 }
