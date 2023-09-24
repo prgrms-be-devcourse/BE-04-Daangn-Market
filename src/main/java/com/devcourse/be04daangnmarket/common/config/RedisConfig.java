f364e3046567efeed140067557f8f22dceda66bb
juwoong kim <wwong7247@gmail.com>
2023-09-25 02:33:20 +0900

    KIM-230 ✨ redis Redisson Client 사용을 위한 설정을 추가하다

--- src/main/java/com/devcourse/be04daangnmarket/common/config/RedisConfig.java
+++ src/main/java/com/devcourse/be04daangnmarket/common/config/RedisConfig.java
@@ -1,5 +1,8 @@
 package com.devcourse.be04daangnmarket.common.config;
 
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,13 +12,14 @@ import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 public class RedisConfig {
+    private static final String REDISSON_HOST_PREFIX = "redis://";
 
     @Value("${spring.cache.redis.host}")
     private String host;
 
     @Value("${spring.cache.redis.port}")
     private int port;
-
+    
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
         return new LettuceConnectionFactory(host, port);
@@ -25,6 +29,17 @@ public class RedisConfig {
     public RedisTemplate<?, ?> redisTemplate() {
         RedisTemplate<?, ?> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory());
+
         return redisTemplate;
+    }
+
+    @Bean
+    public RedissonClient redissonClient() {
+        RedissonClient redisson = null;
+        Config config = new Config();
+        config.useSingleServer().setAddress(REDISSON_HOST_PREFIX + host + ":" + port);
+        redisson = Redisson.create(config);
+
+        return redisson;
     }
 }
