4120a7d8897dea6f46a52f7674e2ae81623dcd9f
kys0411 <32180846@dankook.ac.kr>
2023-09-25 17:05:20 +0900

    KIM-221 ♻️ 공통 url을 추가하다

--- src/main/java/com/devcourse/be04daangnmarket/member/api/SignRestController.java
+++ src/main/java/com/devcourse/be04daangnmarket/member/api/SignRestController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping
+@RequestMapping("/api/v1")
 @RestController
 public class SignRestController {
     private final KakaoService kakaoService;
