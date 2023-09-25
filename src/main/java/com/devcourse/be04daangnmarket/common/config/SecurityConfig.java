572cc33a807c731b9e9067b1976cb78da5021939
kys0411 <32180846@dankook.ac.kr>
2023-09-25 13:05:25 +0900

    KIM-221 🔧 security 설정을 추가하다

--- src/main/java/com/devcourse/be04daangnmarket/common/config/SecurityConfig.java
+++ src/main/java/com/devcourse/be04daangnmarket/common/config/SecurityConfig.java
@@ -4,36 +4,47 @@ import com.devcourse.be04daangnmarket.common.jwt.JwtAuthenticationFilter;
 import com.devcourse.be04daangnmarket.common.jwt.JwtTokenProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-	private final JwtTokenProvider jwtTokenProvider;
+    private final JwtTokenProvider jwtTokenProvider;
 
-	public SecurityConfig(JwtTokenProvider jwtTokenProvider) {
-		this.jwtTokenProvider = jwtTokenProvider;
-	}
+    public SecurityConfig(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
 
-	@Bean
-	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		return http
-			.authorizeHttpRequests(authorizeRequests -> authorizeRequests
-				.requestMatchers(new AntPathRequestMatcher("/**"))
-				.permitAll())
-				.csrf(csrf->csrf.disable())
-				.addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
-			.build();
-	}
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/sign-up").permitAll()
+                                .requestMatchers("/login").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/api/v1/members/**").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/api/v1/comments/**").permitAll()
+                                .anyRequest().authenticated())
 
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+                .csrf(AbstractHttpConfigurer::disable)
+
+                .sessionManagement((sessionManagement) ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
